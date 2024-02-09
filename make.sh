ad5ca6042b52cce19b8edd4cbe7cde11c846725b
Matan1x <moonlandforme@gmail.com>
2024-02-09 13:17:52 +0200

    first working overlay generator

---
+++ make.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+bash src/wayland/xdg-shell.cgen | gcc -c -xc - -o build/xdg-shell.o
+
+bash src/wayland/wlr-layer-shell-unstable-v1.cgen | gcc -c -xc - -o build/zwlr-shell.o
+bash src/wayland/wlr-layer-shell-unstable-v1.hgen build/zwlr-shell.h
+
+gcc -c -iquote build src/a.c -o build/a.o
+gcc -lwayland-client -lcairo build/a.o build/zwlr-shell.o build/xdg-shell.o -o subview
+
+#./a.out
