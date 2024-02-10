74bea9c880f9f692973a3d5f56a6d2fcc845c575
Matan1x <moonlandforme@gmail.com>
2024-02-10 11:43:25 +0200

    start working on frame generation

--- src/wayland/wayland.c
+++ src/wayland/wayland.c
@@ -7,15 +7,16 @@
 #include <sys/mman.h>
 #include "config.h"
 #include <wayland-client.h>
+#include <stdbool.h>
 
 #include "zwlr-shell.h"
-#include "wayland.h"
 #include "config.h"
 
 #define MAP_SIZE WIDTH*HEIGHT*4
 
 void *start_wayland_backend(void *);
 void stop_wayland_backend(void);
+void update_output(void);
 
 typedef struct {
 	struct wl_output *wl_output;
@@ -23,21 +24,30 @@ typedef struct {
 	struct wl_surface *surface;
 	int32_t scale;
 	uint32_t wl_name;
+	_Atomic bool new_data;
 	struct wl_list link;
 } output_t;
 
 static void free_output(output_t *);
+
 static void layer_surface_configure_handler(void *, struct zwlr_layer_surface_v1 *,
                                             uint32_t, uint32_t, uint32_t);
 static void layer_surface_remove_handler(void *, struct zwlr_layer_surface_v1 *);
+
 static void global_handler(void *, struct wl_registry *, uint32_t, const char *,
                            uint32_t);
 static void global_remove_handler(void *, struct wl_registry *, uint32_t);
+
 static void output_scale(void *, struct wl_output *, int32_t);
 static void output_geometry(void *, struct wl_output *, int32_t, int32_t, int32_t,
                             int32_t, int32_t, const char *, const char *, int32_t);
 static void output_mode(void *, struct wl_output *, uint32_t, int32_t, int32_t, int32_t);
 static void output_done(void *, struct wl_output *);
+
+static void frame_done(void *, struct wl_callback *, uint32_t);
+static struct wl_buffer *draw_frame(output_t *output);
+static int allocate_shm_file(size_t);
+static void buffer_release(void *, struct wl_buffer *);
 
 
 static const struct zwlr_layer_surface_v1_listener layer_shell_listener = {
@@ -57,15 +67,23 @@ static const struct wl_registry_listener registry_listener = {
 	.global_remove = global_remove_handler
 };
 
+static const struct wl_callback_listener frame_listener = {
+	.done = frame_done
+};
+
+static const struct wl_buffer_listener buffer_listener = {
+	.release = buffer_release
+};
+
+struct wl_list outputs;
+uint32_t inp;
 static struct wl_display *display;
 static struct wl_registry *registry;
 static struct wl_compositor *compositor;
 static struct wl_shm *shm;
 static struct zwlr_layer_shell_v1 *layer_shell;
-struct wl_list outputs;
 static struct wl_buffer *buffer;
-static uint8_t *mapping;
-static struct wl_shm_pool *pool;
+
 
 static void free_output(output_t *output) {
     if (output->wlr_surf) {
@@ -88,10 +106,19 @@ static void layer_surface_configure_handler(
   uint32_t serial,
   uint32_t width,
   uint32_t height) {
+	output_t *output = data;
 	printf("configure event: w=%u, h=%u, surf=%p, s=%u\n", width, height, surface, serial);
+
 	if (!(width == 0 || width == WIDTH) || !(height == 0 || height == HEIGHT))
-		puts("ERROR: width or height are incorrect");
-	zwlr_layer_surface_v1_ack_configure(surface, serial); // TODO: handle
+		puts("ERROR: width or height are incorrect"); // TODO: handle
+
+	zwlr_layer_surface_v1_ack_configure(surface, serial);
+
+	buffer = draw_frame(output);
+	wl_surface_attach(output->surface, buffer, 0, 0);
+	wl_surface_commit(output->surface);
+
+
 }
 
 static void layer_surface_remove_handler(void *data, struct zwlr_layer_surface_v1 *surface) {
@@ -128,18 +155,16 @@ static void output_done(void *data, struct wl_output *wl_output)
 		wl_region_destroy(input_region);
 		
 		output->wlr_surf = zwlr_layer_shell_v1_get_layer_surface(layer_shell, output->surface, wl_output, ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY, "subtitles");
-		zwlr_layer_surface_v1_add_listener(output->wlr_surf, &layer_shell_listener, NULL);
+		zwlr_layer_surface_v1_add_listener(output->wlr_surf, &layer_shell_listener, output);
 		zwlr_layer_surface_v1_set_size(output->wlr_surf, WIDTH, HEIGHT);
 		zwlr_layer_surface_v1_set_anchor(output->wlr_surf, ZWLR_LAYER_SURFACE_V1_ANCHOR_BOTTOM);
 		zwlr_layer_surface_v1_set_margin(output->wlr_surf, MARGIN_TOP, MARGIN_RIGHT, MARGIN_BOTTOM, MARGIN_LEFT);
 		zwlr_layer_surface_v1_set_keyboard_interactivity(output->wlr_surf, ZWLR_LAYER_SURFACE_V1_KEYBOARD_INTERACTIVITY_NONE);
-		wl_surface_commit(output->surface);
-		wl_display_roundtrip(display);
 	
-	
-		wl_surface_attach(output->surface, buffer, 0, 0);
+		struct wl_callback *cb = wl_surface_frame(output->surface);
+		wl_callback_add_listener(cb, &frame_listener, output);
 		wl_surface_commit(output->surface);
-
+		
 	}
 }
 
@@ -179,18 +204,84 @@ static void global_remove_handler(void *data, struct wl_registry *registry, uint
 			break;
 		}
 	}
-} // TODO: handle
+}
 
+static int allocate_shm_file(size_t size) {
+	int fd = memfd_create("buffer", 0);
+	ftruncate(fd, size);
+	return fd;
+}
+
+static struct wl_buffer *draw_frame(output_t *output)
+{
+    int fd = allocate_shm_file(MAP_SIZE);
+	struct wl_shm_pool *pool;
+    if (fd == -1) {
+        return NULL;
+    }
+
+    uint32_t *data = mmap(NULL, MAP_SIZE,
+            PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (data == MAP_FAILED) {
+        close(fd);
+        return NULL;
+    }
+
+    pool = wl_shm_create_pool(shm, fd, MAP_SIZE);
+    buffer = wl_shm_pool_create_buffer(pool, 0,
+            WIDTH, HEIGHT, WIDTH*4, WL_SHM_FORMAT_ARGB8888);
+    wl_shm_pool_destroy(pool);
+    close(fd);
+
+    /* Draw checkerboxed background */
+    for (int y = 0; y < HEIGHT; ++y) {
+        for (int x = 0; x < WIDTH; ++x) {
+            if ((x + y / 10 * 10) % 20 < 10)
+                data[y * WIDTH + x] = 0xFF666666;
+            else
+                data[y * WIDTH + x] = inp;
+        }
+    }
+
+    munmap(data, MAP_SIZE);
+    wl_buffer_add_listener(buffer, &buffer_listener, NULL);
+    return buffer;
+}
+
+static void buffer_release(void *data, struct wl_buffer *buffer) {
+    wl_buffer_destroy(buffer);
+}
+
+static void frame_done(void *data, struct wl_callback *cb, uint32_t time) {
+	if (cb)
+		wl_callback_destroy(cb);
+
+	output_t *output = data;
+	cb = wl_surface_frame(output->surface);
+	wl_callback_add_listener(cb, &frame_listener, output);
+
+	/* Submit a frame for this event */
+	if (output->new_data) {
+		struct wl_buffer *buffer = draw_frame(output);
+		wl_surface_attach(output->surface, buffer, 0, 0);
+		wl_surface_damage_buffer(output->surface, 0, 0, INT32_MAX, INT32_MAX);
+		output->new_data = false;
+	}
+	wl_surface_commit(output->surface);
+}
+
+void update_output(void) {
+	output_t *output;
+	wl_list_for_each(output, &outputs, link) {
+		printf("%d\n", output->new_data);
+		output->new_data = true;
+	}
+}
 
 void stop_wayland_backend(void) {
 	output_t *output, *tmp;
 	wl_list_for_each_safe(output, tmp, &outputs, link)
 		free_output(output);
-
-	if (mapping)
-		munmap(mapping, MAP_SIZE);
-    if (pool)
-        wl_shm_pool_destroy(pool);
 	if (shm)
 		wl_shm_destroy(shm);
 	if (compositor)
@@ -205,6 +296,7 @@ void stop_wayland_backend(void) {
 
 void *start_wayland_backend(void *arg) {
 	int fd, ret = 0;
+	struct wl_shm_pool *pool;
 	display = wl_display_connect(NULL);
 
 	wl_list_init(&outputs);
@@ -225,20 +317,6 @@ void *start_wayland_backend(void *arg) {
 		ret = -1;
 		goto deallocate;
 	}
-
-	fd = memfd_create("buffer", 0);
-	ftruncate(fd, MAP_SIZE);
-	if (!(mapping = mmap(NULL, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0))) {
-		puts("ERROR: failed to map memory");
-	}
-	pool = wl_shm_create_pool(shm, fd, MAP_SIZE);
-	close(fd);
-	buffer = wl_shm_pool_create_buffer(pool, 0, WIDTH, HEIGHT, 4*WIDTH, WL_SHM_FORMAT_ARGB8888);
-	
-	if (arg)
-		*((uint8_t**)arg) = mapping;
-	else
-		memset(mapping, 0x80, MAP_SIZE);
 
 	while (wl_display_dispatch(display) != -1) {
 	}
