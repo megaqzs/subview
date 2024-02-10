f1fa0dc0fa6bc6c49c8b3f6573336422ae92caca
Matan1x <moonlandforme@gmail.com>
2024-02-10 19:58:45 +0200

    start working on drawing text

--- src/wayland/wayland.h
+++ src/wayland/wayland.h
@@ -4,7 +4,7 @@
 
 //extern struct wl_buffer *buffer;
 extern pthread_mutex_t txt_buf_lock;
-extern uint32_t inp;
+extern char inp[40];
 void *start_wayland_backend(void *);
 void update_output(void);
 void stop_wayland_backend(void);
