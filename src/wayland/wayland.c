8625ba91e8881485089ad9e07cdc96143e440bbb
Matan1x <moonlandforme@gmail.com>
2024-02-13 19:54:30 +0200

    improve code structure
    	modified:   src/wayland/wayland.c

--- src/wayland/wayland.c
+++ src/wayland/wayland.c
@@ -22,13 +22,13 @@ void stop_wayland_backend(void);
 void update_output(void);
 
 typedef struct {
-	struct wl_output *wl_output;
-	struct zwlr_layer_surface_v1 *wlr_surf;
-	struct wl_surface *surface;
-	int32_t scale;
-	uint32_t wl_name;
-	_Atomic bool new_data;
-	struct wl_list link;
+    struct wl_output *wl_output;
+    struct zwlr_layer_surface_v1 *wlr_surf;
+    struct wl_surface *surface;
+    int32_t scale;
+    uint32_t wl_name;
+    _Atomic bool new_data;
+    struct wl_list link;
 } output_t;
 
 static void free_output(output_t *);
@@ -48,27 +48,31 @@ static void frame_done(void *, struct wl_callback *, uint32_t);
 static struct wl_buffer *draw_frame(output_t *output);
 static int allocate_shm_file(size_t);
 static void buffer_release(void *, struct wl_buffer *);
-
+static void free_backend(void);
+static void *_start_wayland_backend(void *);
+void update_output(void);
+void stop_wayland_backend(void);
+int start_wayland_backend(void);
 
 static const struct zwlr_layer_surface_v1_listener layer_shell_listener = {
-	.configure = layer_surface_configure_handler,
-	.closed = layer_surface_remove_handler
+    .configure = layer_surface_configure_handler,
+    .closed = layer_surface_remove_handler
 };
 static const struct wl_output_listener output_listener = {
-	.geometry = output_geometry,
-	.mode = output_mode,
-	.done = output_done,
-	.scale = output_scale,
+    .geometry = output_geometry,
+    .mode = output_mode,
+    .done = output_done,
+    .scale = output_scale,
 };
 static const struct wl_registry_listener registry_listener = {
-	.global = global_handler,
-	.global_remove = global_remove_handler
+    .global = global_handler,
+    .global_remove = global_remove_handler
 };
 static const struct wl_callback_listener frame_listener = {
-	.done = frame_done
+    .done = frame_done
 };
 static const struct wl_buffer_listener buffer_listener = {
-	.release = buffer_release
+    .release = buffer_release
 };
 
 pthread_mutex_t txt_buf_lock;
@@ -95,9 +99,9 @@ static void free_output(output_t *output) {
         wl_surface_destroy(output->surface);
     }
 
-	pthread_mutex_lock(&outputs_lock);
+    pthread_mutex_lock(&outputs_lock);
     wl_list_remove(&output->link);
-	pthread_mutex_unlock(&outputs_lock);
+    pthread_mutex_unlock(&outputs_lock);
     free(output);
 }
 
@@ -107,128 +111,128 @@ static void layer_surface_configure_handler(
   uint32_t serial,
   uint32_t width,
   uint32_t height) {
-	output_t *output = data;
+    output_t *output = data;
 
-	if (!(width == 0 || width == WIDTH) || !(height == 0 || height == HEIGHT))
-		puts("ERROR: width or height are incorrect"); // TODO: handle
+    if (!(width == 0 || width == WIDTH) || !(height == 0 || height == HEIGHT))
+        puts("ERROR: width or height are incorrect"); // TODO: handle
 
-	zwlr_layer_surface_v1_ack_configure(surface, serial);
+    zwlr_layer_surface_v1_ack_configure(surface, serial);
 
-	buffer = draw_frame(output);
-	wl_surface_attach(output->surface, buffer, 0, 0);
-	wl_surface_commit(output->surface);
+    buffer = draw_frame(output);
+    wl_surface_attach(output->surface, buffer, 0, 0);
+    wl_surface_commit(output->surface);
 
 
 }
 
 static void layer_surface_remove_handler(void *data, struct zwlr_layer_surface_v1 *surface) {
-	output_t *output, *tmp;
-	pthread_mutex_lock(&outputs_lock);
-	wl_list_for_each_safe(output, tmp, &outputs, link) {
-		if (output->wlr_surf == surface) {
-			free_output(output);
-			break;
-		}
-	}
-	pthread_mutex_unlock(&outputs_lock);
+    output_t *output, *tmp;
+    pthread_mutex_lock(&outputs_lock);
+    wl_list_for_each_safe(output, tmp, &outputs, link) {
+        if (output->wlr_surf == surface) {
+            free_output(output);
+            break;
+        }
+    }
+    pthread_mutex_unlock(&outputs_lock);
 }
 
 static void output_geometry(void *data, struct wl_output *output, int32_t x,
-							int32_t y, int32_t width_mm, int32_t height_mm, int32_t subpixel,
-							const char *make, const char *model, int32_t transform) {
+                            int32_t y, int32_t width_mm, int32_t height_mm, int32_t subpixel,
+                            const char *make, const char *model, int32_t transform) {
 }
 
 static void output_mode(void *data, struct wl_output *output, uint32_t flags,
-						int32_t width, int32_t height, int32_t refresh) {
+                        int32_t width, int32_t height, int32_t refresh) {
 }
 
 static void output_done(void *data, struct wl_output *wl_output)
 {
-	output_t *output = data;
-	int fd;
+    output_t *output = data;
+    int fd;
 
-	if (!output->wlr_surf) {
-		output->surface = wl_compositor_create_surface(compositor);
-		
-		// Empty input region
-		struct wl_region *input_region =
-			wl_compositor_create_region(compositor);
-		wl_surface_set_input_region(output->surface, input_region);
-		wl_region_destroy(input_region);
-		
-		output->wlr_surf = zwlr_layer_shell_v1_get_layer_surface(layer_shell, output->surface, wl_output, ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY, "subtitles");
-		zwlr_layer_surface_v1_add_listener(output->wlr_surf, &layer_shell_listener, output);
-		zwlr_layer_surface_v1_set_size(output->wlr_surf, WIDTH, HEIGHT);
-		zwlr_layer_surface_v1_set_anchor(output->wlr_surf, ZWLR_LAYER_SURFACE_V1_ANCHOR_BOTTOM);
-		zwlr_layer_surface_v1_set_margin(output->wlr_surf, MARGIN_TOP, MARGIN_RIGHT, MARGIN_BOTTOM, MARGIN_LEFT);
-		zwlr_layer_surface_v1_set_keyboard_interactivity(output->wlr_surf, ZWLR_LAYER_SURFACE_V1_KEYBOARD_INTERACTIVITY_NONE);
-	
-		struct wl_callback *cb = wl_surface_frame(output->surface);
-		wl_callback_add_listener(cb, &frame_listener, output);
-		wl_surface_commit(output->surface);
-		
-	}
+    if (!output->wlr_surf) {
+        output->surface = wl_compositor_create_surface(compositor);
+        
+        // Empty input region
+        struct wl_region *input_region =
+            wl_compositor_create_region(compositor);
+        wl_surface_set_input_region(output->surface, input_region);
+        wl_region_destroy(input_region);
+        
+        output->wlr_surf = zwlr_layer_shell_v1_get_layer_surface(layer_shell, output->surface, wl_output, ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY, "subtitles");
+        zwlr_layer_surface_v1_add_listener(output->wlr_surf, &layer_shell_listener, output);
+        zwlr_layer_surface_v1_set_size(output->wlr_surf, WIDTH, HEIGHT);
+        zwlr_layer_surface_v1_set_anchor(output->wlr_surf, ZWLR_LAYER_SURFACE_V1_ANCHOR_BOTTOM);
+        zwlr_layer_surface_v1_set_margin(output->wlr_surf, MARGIN_TOP, MARGIN_RIGHT, MARGIN_BOTTOM, MARGIN_LEFT);
+        zwlr_layer_surface_v1_set_keyboard_interactivity(output->wlr_surf, ZWLR_LAYER_SURFACE_V1_KEYBOARD_INTERACTIVITY_NONE);
+    
+        struct wl_callback *cb = wl_surface_frame(output->surface);
+        wl_callback_add_listener(cb, &frame_listener, output);
+        wl_surface_commit(output->surface);
+        
+    }
 }
 
 
 static void output_scale(void *data, struct wl_output *wl_output, int32_t scale)
 {
-	output_t *output = data;
-	output->scale = scale;
+    output_t *output = data;
+    output->scale = scale;
 }
 
 static void global_handler(void *data, struct wl_registry *registry, uint32_t name, const char *interface, uint32_t version) {
-	struct wl_output_ *output;
-	if (strcmp(interface, wl_compositor_interface.name) == 0) {
-		compositor = wl_registry_bind(registry, name,
-			&wl_compositor_interface, 4);
-	} else if (strcmp(interface, wl_shm_interface.name) == 0) {
-		shm = wl_registry_bind(registry, name,
-			&wl_shm_interface, 1);
-	} else if (strcmp(interface, zwlr_layer_shell_v1_interface.name) == 0) {
-		layer_shell = wl_registry_bind(registry, name,
-			&zwlr_layer_shell_v1_interface, 1);
-	} else if (strcmp(interface, wl_output_interface.name) == 0) {
-		output_t *output = calloc(1, sizeof(output_t));
-		output->wl_output = wl_registry_bind(registry, name,
-			&wl_output_interface, 2);
-		output->wl_name = name;
-		wl_output_add_listener(output->wl_output, &output_listener, output);
-		pthread_mutex_lock(&outputs_lock);
-		wl_list_insert(&outputs, &output->link);
-		pthread_mutex_unlock(&outputs_lock);
-	}
+    struct wl_output_ *output;
+    if (strcmp(interface, wl_compositor_interface.name) == 0) {
+        compositor = wl_registry_bind(registry, name,
+            &wl_compositor_interface, 4);
+    } else if (strcmp(interface, wl_shm_interface.name) == 0) {
+        shm = wl_registry_bind(registry, name,
+            &wl_shm_interface, 1);
+    } else if (strcmp(interface, zwlr_layer_shell_v1_interface.name) == 0) {
+        layer_shell = wl_registry_bind(registry, name,
+            &zwlr_layer_shell_v1_interface, 1);
+    } else if (strcmp(interface, wl_output_interface.name) == 0) {
+        output_t *output = calloc(1, sizeof(output_t));
+        output->wl_output = wl_registry_bind(registry, name,
+            &wl_output_interface, 2);
+        output->wl_name = name;
+        wl_output_add_listener(output->wl_output, &output_listener, output);
+        pthread_mutex_lock(&outputs_lock);
+        wl_list_insert(&outputs, &output->link);
+        pthread_mutex_unlock(&outputs_lock);
+    }
 }
 
 static void global_remove_handler(void *data, struct wl_registry *registry, uint32_t name) {
-	output_t *output, *tmp;
-	pthread_mutex_lock(&outputs_lock);
-	wl_list_for_each_safe(output, tmp, &outputs, link) {
-		if (output->wl_name == name) {
-			free_output(output);
-			break;
-		}
-	}
-	pthread_mutex_unlock(&outputs_lock);
+    output_t *output, *tmp;
+    pthread_mutex_lock(&outputs_lock);
+    wl_list_for_each_safe(output, tmp, &outputs, link) {
+        if (output->wl_name == name) {
+            free_output(output);
+            break;
+        }
+    }
+    pthread_mutex_unlock(&outputs_lock);
 }
 
 static int allocate_shm_file(size_t size) {
-	int fd = memfd_create("buffer", 0);
-	ftruncate(fd, size);
-	return fd;
+    int fd = memfd_create("buffer", 0);
+    ftruncate(fd, size);
+    return fd;
 }
 
 static struct wl_buffer *draw_frame(output_t *output)
 {
-	int fd = allocate_shm_file(MAP_SIZE);
-	cairo_surface_t *cr_surf;
-	cairo_t *cr;
-	PangoLayout *layout;
-	PangoFontDescription *desc;
-	PangoRectangle extents;
-	double x,y;
+    int fd = allocate_shm_file(MAP_SIZE);
+    cairo_surface_t *cr_surf;
+    cairo_t *cr;
+    PangoLayout *layout;
+    PangoFontDescription *desc;
+    PangoRectangle extents;
+    double x,y;
 
-	struct wl_shm_pool *pool;
+    struct wl_shm_pool *pool;
     if (fd == -1) {
         return NULL;
     }
@@ -256,44 +260,44 @@ static struct wl_buffer *draw_frame(output_t *output)
     //    }
     //}
 
-	if (inp) {
-		cr_surf = cairo_image_surface_create_for_data(data, CAIRO_FORMAT_ARGB32, WIDTH, HEIGHT, WIDTH*4);
-		cr = cairo_create(cr_surf);
-		cairo_surface_destroy(cr_surf);
+    if (inp) {
+        cr_surf = cairo_image_surface_create_for_data(data, CAIRO_FORMAT_ARGB32, WIDTH, HEIGHT, WIDTH*4);
+        cr = cairo_create(cr_surf);
+        cairo_surface_destroy(cr_surf);
 
-		// initialize layout
-		desc = pango_font_description_from_string(FONT_DESC);
-		layout = pango_cairo_create_layout(cr);
-		pango_layout_set_font_description(layout, desc);
+        // initialize layout
+        desc = pango_font_description_from_string(FONT_DESC);
+        layout = pango_cairo_create_layout(cr);
+        pango_layout_set_font_description(layout, desc);
 
-		// write the input to the layout without the input changing
-		pthread_mutex_lock(&txt_buf_lock);
-		pango_layout_set_text(layout, inp, -1);
-		pthread_mutex_unlock(&txt_buf_lock);
-		pango_layout_set_width(layout, pango_units_from_double(WIDTH));
+        // write the input to the layout without the input changing
+        pthread_mutex_lock(&txt_buf_lock);
+        pango_layout_set_text(layout, inp, -1);
+        pthread_mutex_unlock(&txt_buf_lock);
+        pango_layout_set_width(layout, pango_units_from_double(WIDTH));
 
 
-		// get text positioning information
-		pango_layout_get_extents(layout, NULL, &extents);
-		x = WIDTH/2 - pango_units_to_double(extents.width/2);
-		y = HEIGHT - pango_units_to_double(extents.height);
+        // get text positioning information
+        pango_layout_get_extents(layout, NULL, &extents);
+        x = WIDTH/2 - pango_units_to_double(extents.width/2);
+        y = HEIGHT - pango_units_to_double(extents.height);
 
-		// draw background behind text
-		cairo_set_source_rgba(cr, 0.2, 0.2, 0.2, 0.7);
-		cairo_rectangle(cr,
-				x-OVERSCAN_X,
-				y-OVERSCAN_Y,
-				pango_units_to_double(extents.width)+2*OVERSCAN_X,
-				pango_units_to_double(extents.height)+2*OVERSCAN_Y);
-		cairo_fill(cr);
+        // draw background behind text
+        cairo_set_source_rgba(cr, 0.2, 0.2, 0.2, 0.7);
+        cairo_rectangle(cr,
+                x-OVERSCAN_X,
+                y-OVERSCAN_Y,
+                pango_units_to_double(extents.width)+2*OVERSCAN_X,
+                pango_units_to_double(extents.height)+2*OVERSCAN_Y);
+        cairo_fill(cr);
 
-		// draw text
-		cairo_move_to(cr, x-pango_units_to_double(extents.x), y-pango_units_to_double(extents.y));
-		cairo_set_source_rgba(cr, 1, 1, 1, 1);
-		pango_cairo_show_layout(cr, layout);
+        // draw text
+        cairo_move_to(cr, x-pango_units_to_double(extents.x), y-pango_units_to_double(extents.y));
+        cairo_set_source_rgba(cr, 1, 1, 1, 1);
+        pango_cairo_show_layout(cr, layout);
 
-		cairo_destroy(cr);
-	}
+        cairo_destroy(cr);
+    }
 
     munmap(data, MAP_SIZE);
     wl_buffer_add_listener(buffer, &buffer_listener, NULL);
@@ -305,100 +309,103 @@ static void buffer_release(void *data, struct wl_buffer *buffer) {
 }
 
 static void frame_done(void *data, struct wl_callback *cb, uint32_t time) {
-	if (cb)
-		wl_callback_destroy(cb);
+    if (cb)
+        wl_callback_destroy(cb);
 
-	output_t *output = data;
-	cb = wl_surface_frame(output->surface);
-	wl_callback_add_listener(cb, &frame_listener, output);
+    output_t *output = data;
+    cb = wl_surface_frame(output->surface);
+    wl_callback_add_listener(cb, &frame_listener, output);
 
-	/* Submit a frame for this event */
-	if (output->new_data) {
-		struct wl_buffer *buffer = draw_frame(output);
-		wl_surface_attach(output->surface, buffer, 0, 0);
-		wl_surface_damage_buffer(output->surface, 0, 0, INT32_MAX, INT32_MAX);
-		output->new_data = false;
-	}
-	wl_surface_commit(output->surface);
+    /* Submit a frame for this event */
+    if (output->new_data) {
+        struct wl_buffer *buffer = draw_frame(output);
+        wl_surface_attach(output->surface, buffer, 0, 0);
+        wl_surface_damage_buffer(output->surface, 0, 0, INT32_MAX, INT32_MAX);
+        output->new_data = false;
+    }
+    wl_surface_commit(output->surface);
+}
+
+static void free_backend(void) {
+    output_t *output, *tmp;
+    wl_list_for_each_safe(output, tmp, &outputs, link)
+        free_output(output);
+    if (shm) {
+        wl_shm_destroy(shm);
+        shm = NULL;
+    }
+    if (compositor) {
+        wl_compositor_destroy(compositor);
+        compositor = NULL;
+    }
+    if (layer_shell) {
+        zwlr_layer_shell_v1_destroy(layer_shell);
+        layer_shell = NULL;
+    }
+    if (registry) {
+        wl_registry_destroy(registry);
+        registry = NULL;
+    }
+    if (display) {
+        wl_display_disconnect(display);
+        display = NULL;
+    }
+    pthread_mutex_destroy(&txt_buf_lock);
+    pthread_mutex_destroy(&outputs_lock);
+}
+
+static void *_start_wayland_backend(void *arg) {
+    int status;
+    while (!closed && (status = wl_display_dispatch(display)) != -1) {
+    } // only works because of frame events that make wl_display_dispatch not block forever for sure
+    // TODO: make it work without frame events, in case of no output
+    if (status == -1)
+        puts("ERROR: failed to dispatch events");
+    free_backend();
 }
 
 void update_output(void) {
-	output_t *output;
+    output_t *output;
 
-	pthread_mutex_lock(&outputs_lock);
-	wl_list_for_each(output, &outputs, link) {
-		output->new_data = true;
-	}
-	pthread_mutex_unlock(&outputs_lock);
+    pthread_mutex_lock(&outputs_lock);
+    wl_list_for_each(output, &outputs, link) {
+        output->new_data = true;
+    }
+    pthread_mutex_unlock(&outputs_lock);
 }
 
 void stop_wayland_backend(void) {
-	closed = true;
-}
-
-void *_start_wayland_backend(void *arg) {
-	int status;
-	while (!closed && (status = wl_display_dispatch(display)) != -1) {
-	} // only works because of frame events that make wl_display_dispatch not block forever for sure
-	// TODO: make it work without frame events, in case of no output
-	if (status == -1)
-		puts("ERROR: failed to dispatch events");
-	output_t *output, *tmp;
-	wl_list_for_each_safe(output, tmp, &outputs, link)
-		free_output(output);
-	if (shm) {
-		wl_shm_destroy(shm);
-		shm = NULL;
-	}
-	if (compositor) {
-		wl_compositor_destroy(compositor);
-		compositor = NULL;
-	}
-	if (layer_shell) {
-		zwlr_layer_shell_v1_destroy(layer_shell);
-		layer_shell = NULL;
-	}
-	if (registry) {
-		wl_registry_destroy(registry);
-		registry = NULL;
-	}
-	if (display) {
-		wl_display_disconnect(display);
-		display = NULL;
-	}
-	pthread_mutex_destroy(&txt_buf_lock);
-	pthread_mutex_destroy(&outputs_lock);
+    closed = true;
 }
 
 int start_wayland_backend(void) {
-	int ret = 0;
-	pthread_t inf;
-	pthread_mutex_init(&outputs_lock, NULL);
-	pthread_mutex_init(&txt_buf_lock, NULL);
+    pthread_t inf;
+    pthread_mutex_init(&outputs_lock, NULL);
+    pthread_mutex_init(&txt_buf_lock, NULL);
 
-	wl_list_init(&outputs);
-	display = wl_display_connect(NULL);
+    wl_list_init(&outputs);
+    display = wl_display_connect(NULL);
 
-	if (display == NULL) {
-		puts("ERROR: failed to connect to compositor");
-		goto deallocate;
-	}
+    if (display == NULL) {
+        puts("ERROR: failed to connect to compositor");
+        free_backend();
+        return -3;
+    }
 
-	registry = wl_display_get_registry(display);
-	wl_registry_add_listener(registry, &registry_listener, NULL);
-	wl_display_roundtrip(display); // wait for the "initial" set of globals to appear
+    registry = wl_display_get_registry(display);
+    wl_registry_add_listener(registry, &registry_listener, NULL);
+    wl_display_roundtrip(display); // wait for the "initial" set of globals to appear
 
-	// all our objects should be ready!
-	if (!compositor || !shm || !layer_shell) {
-		printf("ERROR: some wayland globals weren't found [%p, %p, %p]\n", compositor, shm, layer_shell);
-		goto deallocate;
-	}
+    // all our objects should be ready!
+    if (!compositor || !shm || !layer_shell) {
+        printf("ERROR: some wayland globals weren't found [%p, %p, %p]\n", compositor, shm, layer_shell);
+        free_backend();
+        return -2;
+    }
 
-	if (!pthread_create(&inf, NULL, _start_wayland_backend, NULL))
-		goto exit;
+    if (!pthread_create(&inf, NULL, _start_wayland_backend, NULL))
+        return 0;
 
-deallocate:
-	stop_wayland_backend();
-exit:
-	return ret;
+    free_backend();
+    return -1;
 }
