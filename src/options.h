0da156c58e3e705aacac86aefad913b521cb792b
Matan1x <moonlandforme@gmail.com>
2024-02-14 16:25:46 +0200

    add subtitle drawing options

--- src/options.h
+++ src/options.h
@@ -7,7 +7,6 @@ enum text_style {
     PLAIN, // only displays the text without any background
     BLOCK, // displays the text in front of a box for the entire input
     LINE_BLOCK, // displays the text in front of a box for every line
-    BORDER, // adds a border around the text based on it's shape
     FILL // fills the whole buffer with background, for debuging
 };
 
@@ -41,12 +40,9 @@ typedef struct {
     char *font_desc;
     bool align_center; 
     
-    // only applies for border style
-    uint32_t border_strength;
-
     // only applies for paragraph_block and line_block styles
-    uint32_t overscan_x;
-    uint32_t overscan_y;
+    int32_t overscan_x;
+    int32_t overscan_y;
 } options_t;
 
 int parse_args(int argc, char *argv[], options_t *);
