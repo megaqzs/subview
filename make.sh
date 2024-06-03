309e1c9bc5e14b5d76db340fac03cf01c8628c47
megaqzs <91798921+megaqzs@users.noreply.github.com>
2024-06-03 08:01:26 +0300

    rename main file

--- make.sh
+++ make.sh
@@ -8,7 +8,7 @@ bash src/wayland/xdg-shell.cgen | gcc -c -xc - -o build/xdg-shell.o
 bash src/wayland/wlr-layer-shell-unstable-v1.cgen | gcc -c -xc - -o build/zwlr-shell.o
 bash src/wayland/wlr-layer-shell-unstable-v1.hgen build/zwlr-shell.h
 
-gcc -c -iquote build src/a.c -o build/a.o
-gcc -lwayland-client -lcairo build/a.o build/zwlr-shell.o build/xdg-shell.o -o subview
+gcc -c -iquote build src/subview.c -o build/subview.o
+gcc -lwayland-client -lcairo build/subview.o build/zwlr-shell.o build/xdg-shell.o -o subview
 
 #./a.out
