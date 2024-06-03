91786e8b14702a530277ed4ac26b206da6a37425
megaqzs <91798921+megaqzs@users.noreply.github.com>
2024-06-03 08:01:28 +0300

    basic implementation improvments

--- src/subview.c
+++ src/subview.c
@@ -11,16 +11,23 @@
 #define MAP_SIZE WIDTH*HEIGHT*4
 
 int main(int argc, char *argv[]) {
+	size_t len = 0;
 	pthread_t inf;
 	if (!pthread_create(&inf, NULL, start_wayland_backend, NULL)) {
 		pthread_mutex_lock(&txt_buf_lock);
-		while (fgets(inp, sizeof(inp), stdin)) {
-				inp[strlen(inp)-1] = '\0';
-				pthread_mutex_unlock(&txt_buf_lock);
-				update_output();
-				pthread_mutex_lock(&txt_buf_lock);
+		while (getline(&inp, &len, stdin) > 0) {
+			int ilen = strlen(inp)-1;
+			if (inp[ilen] == '\n' || inp[ilen] == '\r') {
+				inp[ilen--] = '\0';
+				if (inp[ilen] == '\n' || inp[ilen] == '\r')
+					inp[ilen--] = '\0';
+			}
+
+			pthread_mutex_unlock(&txt_buf_lock);
+			update_output();
+			pthread_mutex_lock(&txt_buf_lock);
 		}
-		pthread_mutex_unlock(&txt_buf_lock);
+		free(inp);
 		stop_wayland_backend();
 	}
 		
