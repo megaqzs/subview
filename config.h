b42407f87ebf51315cccb4065a527d4f51cc9e1f
megaqzs <91798921+megaqzs@users.noreply.github.com>
2024-06-03 08:01:28 +0300

    use pango to add bidi and start supporting line wrapping

--- config.h
+++ config.h
@@ -5,9 +5,6 @@
 #define MARGIN_RIGHT 0
 #define MARGIN_LEFT 0
 
-#define FONT "Sans"
-#define FONT_SIZE 52.0
-#define FONT_WEIGHT CAIRO_FONT_WEIGHT_NORMAL
-#define FONT_SLANT CAIRO_FONT_SLANT_NORMAL
-#define OVERSCAN_X 20
-#define OVERSCAN_Y 20
+#define FONT_DESC "Sans 52"
+#define OVERSCAN_X 10
+#define OVERSCAN_Y 0
