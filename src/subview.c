6b93385901a282bc4aebe4d613e0bc9c2fc69688
megaqzs <91798921+megaqzs@users.noreply.github.com>
2024-06-03 08:01:27 +0300

    start working on frame generation

--- src/subview.c
+++ src/subview.c
@@ -3,15 +3,22 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
 
 #include "config.h"
 #include "wayland/wayland.h"
 
+#define MAP_SIZE WIDTH*HEIGHT*4
+
 int main(int argc, char *argv[]) {
 	pthread_t inf;
-	uint8_t *mapping;
-	if (!pthread_create(&inf, NULL, start_wayland_backend, (void*) NULL)) {
-		getc(stdin);
+	if (!pthread_create(&inf, NULL, start_wayland_backend, NULL)) {
+		while (scanf("%08x", &inp)) {
+				update_output();
+
+			while (getc(stdin) != '\n');
+		}
 		stop_wayland_backend();
 	}
 		
--- src/wayland/wayland.h
+++ src/wayland/wayland.h
@@ -1,6 +1,11 @@
 #ifndef WAYLAND_WAYLAND_H
 #include <wayland-client.h>
-void *start_wayland_backend(void *arg);
+
+//extern struct wl_buffer *buffer;
+extern uint32_t inp;
+void *start_wayland_backend(void *);
+void update_output(void);
 void stop_wayland_backend(void);
+
 #define WAYLAND_WAYLAND_H
 #endif
