49913065ae5f30aeae8f4ccabedea9be880b39b8
megaqzs <91798921+megaqzs@users.noreply.github.com>
2024-06-03 08:01:29 +0300

    improve code structure
    	modified:   src/wayland/wayland.c

--- src/subview.c
+++ src/subview.c
@@ -11,32 +11,31 @@
 #define MAP_SIZE WIDTH*HEIGHT*4
 
 int main(int argc, char *argv[]) {
-	char *line;
-	size_t len = 0;
-	if (!start_wayland_backend()) {
-		while (getline(&line, &len, stdin) > 0) {
-			int ilen = strlen(line);
-			if (line[ilen-1] == '\n' || line[ilen-1] == '\r') {
-				line[--ilen] = '\0';
-				if (line[ilen-1] == '\n' || line[ilen-1] == '\r')
-					line[--ilen] = '\0';
-			}
-			if (!ilen)
-				break;
+    char *line;
+    size_t len = 0;
+    if (!start_wayland_backend()) {
+        while (getline(&line, &len, stdin) > 0) {
+            int ilen = strlen(line);
+            if (line[ilen-1] == '\n' || line[ilen-1] == '\r') {
+                line[--ilen] = '\0';
+                if (line[ilen-1] == '\n' || line[ilen-1] == '\r')
+                    line[--ilen] = '\0';
+            }
+            if (!ilen)
+                break;
 
-			pthread_mutex_lock(&txt_buf_lock);
-			free(inp);
-			inp = malloc(ilen+1);
-			memcpy(inp, line, ilen+1);
-			update_output();
-			pthread_mutex_unlock(&txt_buf_lock);
-		}
-		free(line);
-		stop_wayland_backend();
-		pthread_mutex_lock(&txt_buf_lock);
-		free(inp);
-		inp = NULL;
-		pthread_mutex_unlock(&txt_buf_lock);
-	}
-		
+            pthread_mutex_lock(&txt_buf_lock);
+            free(inp);
+            inp = malloc(ilen+1);
+            memcpy(inp, line, ilen+1);
+            update_output();
+            pthread_mutex_unlock(&txt_buf_lock);
+        }
+        free(line);
+        stop_wayland_backend();
+        pthread_mutex_lock(&txt_buf_lock);
+        free(inp);
+        inp = NULL;
+        pthread_mutex_unlock(&txt_buf_lock);
+    }
 }
