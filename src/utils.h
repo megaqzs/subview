3965fb7f3645ccca6a9ba418435b45f172997716
megaqzs <91798921+megaqzs@users.noreply.github.com>
2024-06-03 08:01:31 +0300

    add program name prefix to logging format

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,12 @@
+#define MESSAGE_NAME_PREFIX "subview: "
+
 #define _STRINGIFY2(x) #x
 #define STRINGIFY(x) _STRINGIFY2(x)
-#define MESSAGE(stream, lvl, name, M, ...) do { if (log_level <= (lvl)) fprintf(stream, name ": " __FILE__ ":" STRINGIFY(__LINE__) ": " M "\n", ## __VA_ARGS__); } while (0)
+
+#define MESSAGE(stream, lvl, name, M, ...) do {\
+    if (log_level <= (lvl))\
+        fprintf(stream, MESSAGE_NAME_PREFIX name ": " __FILE__ ":" STRINGIFY(__LINE__) ": " M "\n", ## __VA_ARGS__);\
+} while (0)
 
 #define PDEBUG(M, ...) MESSAGE(stderr, DEBUG, "debug", M, ## __VA_ARGS__)
 #define PINFO(M, ...) MESSAGE(stderr, INFO, "info", M, ## __VA_ARGS__)
