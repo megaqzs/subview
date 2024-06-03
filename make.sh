3ca3dcf7c0bbb3a963c770a7e67e00ebe4ffca71
megaqzs <91798921+megaqzs@users.noreply.github.com>
2024-06-03 08:01:28 +0300

    improve make system

--- make.sh
+++ make.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+mkdir -p build
 cp -n config.h build/config.h
 set -e
 
-bash src/wayland/xdg-shell.cgen | gcc -c -xc - -o build/xdg-shell.o
+PKGS="wayland-client cairo"
+LDFLAGS="$LDFLAGS $(pkg-config --libs $PKGS)"
+CFLAGS="$CFLAGS $(pkg-config --cflags $PKGS)"
 
-bash src/wayland/wlr-layer-shell-unstable-v1.cgen | gcc -c -xc - -o build/zwlr-shell.o
+bash src/wayland/xdg-shell.cgen | gcc -c -xc - $CFLAGS -o build/xdg-shell.o
+
+bash src/wayland/wlr-layer-shell-unstable-v1.cgen | gcc -c -xc - $CFLAGS -o build/zwlr-shell.o
 bash src/wayland/wlr-layer-shell-unstable-v1.hgen build/zwlr-shell.h
 
-gcc -c -iquote build src/wayland/wayland.c -o build/wayland.o
-gcc -c -iquote build src/subview.c  -o build/subview.o
-gcc -lcairo -lwayland-client build/subview.o build/zwlr-shell.o build/xdg-shell.o build/wayland.o -o subview
+gcc -c -iquote build src/wayland/wayland.c $CFLAGS -o build/wayland.o
+gcc -c -iquote build src/subview.c $CFLAGS -o build/subview.o
+gcc build/subview.o build/zwlr-shell.o build/xdg-shell.o build/wayland.o $LDFLAGS -o subview
 
 #./a.out
